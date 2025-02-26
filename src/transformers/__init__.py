ce0bfbf8dfb41c64cf1f0c50f56b34db2b38745a
Alexander Hagele <alexanderhagele@gmail.com>
2025-02-26 14:23:35 +0100

    init swissai model

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -836,6 +836,7 @@ _import_structure = {
     "models.swin": ["SwinConfig"],
     "models.swin2sr": ["Swin2SRConfig"],
     "models.swinv2": ["Swinv2Config"],
+    "models.swissai": ["SwissAIConfig"],
     "models.switch_transformers": ["SwitchTransformersConfig"],
     "models.t5": ["T5Config"],
     "models.table_transformer": ["TableTransformerConfig"],
@@ -3792,6 +3793,13 @@ else:
             "Swinv2PreTrainedModel",
         ]
     )
+    _import_structure["models.swissai"].extend(
+        [
+            "SwissAIModel",
+            "SwissAIConfig",
+            "SwissAIPreTrainedModel",
+        ]
+    )
     _import_structure["models.switch_transformers"].extend(
         [
             "SwitchTransformersEncoderModel",
@@ -6133,6 +6141,9 @@ if TYPE_CHECKING:
     from .models.swin import SwinConfig
     from .models.swin2sr import Swin2SRConfig
     from .models.swinv2 import Swinv2Config
+    from .models.swissai import (
+        SwissAIConfig,
+    )
     from .models.switch_transformers import (
         SwitchTransformersConfig,
     )
@@ -8608,6 +8619,11 @@ if TYPE_CHECKING:
             Swinv2ForMaskedImageModeling,
             Swinv2Model,
             Swinv2PreTrainedModel,
+        )
+        from .models.swissai import (
+            SwissAIModel,
+            SwissAIConfig,
+            SwissAIPreTrainedModel,
         )
         from .models.switch_transformers import (
             SwitchTransformersEncoderModel,
